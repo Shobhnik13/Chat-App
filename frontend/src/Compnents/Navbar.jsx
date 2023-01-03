282401d3b77de46f4a978d03dacb5568883b3719
shobhnik13 <shobhnikw@gmail.com>
2023-01-04 04:13:49 +0530

    added google log in functionality

--- frontend/src/Compnents/Navbar.jsx
+++ frontend/src/Compnents/Navbar.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
+import {auth} from '../Firebase'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import Signin from './Signin'
 const style={
     nav:'flex bg-gray-800 h-20 p-2 items-center justify-between',
     heading:'text-white text-3xl'
 }
 const Navbar = () => {
-  return (
+  const [user]=useAuthState(auth)
+    return (
     <div className={style.nav}>
             <h1 className={style.heading}>Chat App</h1>
+            <Signin />
     </div>
   )
 }
---
+++ frontend/src/Compnents/Signin.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import GoogleButton from 'react-google-button'
+import {auth} from '../Firebase'
+import { GoogleAuthProvider,signInWithRedirect,signInWithPopup } from 'firebase/auth'
+const style={
+     
+}
+const googleSignIn=()=>{
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+}
+const Signin = () => {
+  return (
+    <div>
+    <GoogleButton onClick={googleSignIn}/>
+    </div>
+  )
+}
+
+export default Signin
